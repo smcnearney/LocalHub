706669711ae4dbe0ee74f3efe53948cf3578848c
Stephen McNearney <stephen@hownow.media>
2021-01-18 16:20:14 -0500

    New commit that uses OpenStreetMap API, OpenCageData API, and PredictHQ API, going to start a new branch and consolidate to just OpenCageData API and Ticketmaster

--- js/scripts.js
+++ js/scripts.js
@@ -35,9 +35,51 @@ function geoFindMe() {
   
 document.querySelector('#find-me').addEventListener('click', geoFindMe);
 
+function geohash() {
+    var apikey = '35fcb69df81c4885ba7dd1b123aa7fc5';
+    var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+    var request_url = api_url
+    + '?'
+    + 'key=' + apikey
+    + `&q=' + ${latitude} + ',' + ${longitude}`
+    + '&pretty=1'
+  
 
+    // see full list of required and optional parameters:
+    // https://opencagedata.com/api#forward
 
-const characterButton = document.getElementById("eventsButton");
+    var request = new XMLHttpRequest();
+    request.open('GET', request_url, true);
+
+    request.onload = function() {
+    // see full list of possible response codes:
+    // https://opencagedata.com/api#codes
+
+    if (request.status === 200){ 
+        // Success!
+        var data = JSON.parse(request.responseText);
+        //alert(data.results[0].geohash); // print the location
+        console.log(data.results.annotations['10']);
+
+    } else if (request.status <= 500){ 
+        // We reached our target server, but it returned an error            
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log('error msg: ' + data.status.message);
+    } else {
+        console.log("server error");
+    }
+    };
+
+    request.onerror = function() {
+    console.log("unable to connect to server");        
+    };
+
+    request.send();  // make the request
+}
+geohash()
+
+const eventsButton = document.getElementById("eventsButton");
 
 eventsButton.addEventListener("click", function (event) {
     event.preventDefault(); //prevents submit action which would reload page 
