ec10888805eccc307df3acd4000b2a559bdf67d2
Stephen McNearney <stephen@hownow.media>
2021-01-18 18:27:47 -0500

    Successfully gets user location and returns events from ticketmaster using OpenCageData API and Ticketmaster API - still needs work as far as styling and HTML - perhaps we should make separate HTML pages for different 'categories' of events?

--- js/scripts.js
+++ js/scripts.js
@@ -1,132 +1,106 @@
-'use strict';
+// browser location starts when this function is called 
+let geohash;
 
-let latitude
-let longitude
+function getLocation() { 
 
-function geoFindMe() {
-
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
+    // check to make sure geolocation is possible
+    if (navigator.geolocation) { 
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    } else { 
+      console.log('Geolocation is not supported')}; 
+    } 
   
-    mapLink.href = '';
-    mapLink.textContent = '';
   
-    function success(position) {
-      latitude  = position.coords.latitude;
-      longitude = position.coords.longitude;
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
     }
   
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
+    function success(pos) {
   
-    if(!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
+        var query = pos.coords.latitude + ',' + pos.coords.longitude;
+        console.log('coordinates: ' + query);
+        console.log('accuracy: ' + pos.coords.accuracy + ' meters.');
   
-  }
+    // now we have coordinates, it is time to use them to  
+    // do some reverse geocoding to get back the location information
+        var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+        var apikey = '35fcb69df81c4885ba7dd1b123aa7fc5';
   
-document.querySelector('#find-me').addEventListener('click', geoFindMe);
-
-function geohash() {
-    var apikey = '35fcb69df81c4885ba7dd1b123aa7fc5';
-    var api_url = 'https://api.opencagedata.com/geocode/v1/json'
-    var request_url = api_url
-    + '?'
-    + 'key=' + apikey
-    + `&q=' + ${latitude} + ',' + ${longitude}`
-    + '&pretty=1'
+        var request_url = api_url
+        + '?'
+        + 'key=' + apikey
+        + '&q=' + encodeURIComponent(query)
+        + '&pretty=1'
   
-
-    // see full list of required and optional parameters:
-    // https://opencagedata.com/api#forward
-
-    var request = new XMLHttpRequest();
-    request.open('GET', request_url, true);
-
-    request.onload = function() {
-    // see full list of possible response codes:
-    // https://opencagedata.com/api#codes
-
-    if (request.status === 200){ 
-        // Success!
-        var data = JSON.parse(request.responseText);
-        //alert(data.results[0].geohash); // print the location
-        console.log(data.results.annotations['10']);
-
-    } else if (request.status <= 500){ 
-        // We reached our target server, but it returned an error            
-        console.log("unable to geocode! Response code: " + request.status);
-        var data = JSON.parse(request.responseText);
-        console.log('error msg: ' + data.status.message);
-    } else {
-        console.log("server error");
-    }
+    // now we follow the steps in the OpenCage javascript tutorial 
+    // full example:
+    // https://opencagedata.com/tutorials/geocode-in-javascript
+  
+        var request = new XMLHttpRequest();
+        request.open('GET', request_url, true);
+  
+        request.onload = function() {
+      // see full list of possible response codes:
+      // https://opencagedata.com/api#codes
+  
+        if (request.status === 200){  // Success!
+            var data = JSON.parse(request.responseText);
+            geohash = data.results[0].annotations.geohash.substring(0,9);
+            console.log(geohash);
+        } 
+        else if (request.status <= 500){ // We reached our target server, but it returned an error                   
+            console.log("unable to geocode! Response code: " + request.status);
+            var data = JSON.parse(request.responseText);
+            console.log('error msg: ' + data.status.message);
+        } 
+        else {
+            console.log("server error");
+        }
     };
-
+    
     request.onerror = function() {
-    console.log("unable to connect to server");        
+      // There was a connection error of some sort
+      console.log("unable to connect to server");
     };
-
+    
     request.send();  // make the request
+  
 }
-geohash()
 
-const eventsButton = document.getElementById("eventsButton");
+locationButton.addEventListener("click", function (event) {
+    event.preventDefault(); //prevents submit action which would reload page
+    getLocation();
+});
 
-eventsButton.addEventListener("click", function (event) {
-    event.preventDefault(); //prevents submit action which would reload page 
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer YFdYgO1Va5J6Q_U9Dxrbl7wv4ajE146Tr7hqdjZ3");
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
 
-    fetch("https://api.predicthq.com/v1/events", requestOptions)
+function getEvents() {
+    const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=kPwc8hRP4lHymySkz8uZDaL5OUA2dXYh&geoPoint=${geohash}&radius=20`;
+    fetch(url)
     .then(function (response) {
         return response.json();
     })
-    .then(function (data){
-        var eventList = data.results.map((individualEvent) => {
+    .then(function (json) {
+        var eventList = json._embedded.events.map((individualEvent) => {
             return `<div>
                         <ul>
-                            <li>Name: ${individualEvent.title}</li>
-                            <li>Description: ${individualEvent.description}</li>
-                            <li>Category: ${individualEvent.category}</li>
+                            <li><img src="${individualEvent.images[0].url}" /></li>  
+                            <li>Name: ${individualEvent.name}</li>
+                            <li>Date: ${individualEvent.dates.start.localDate}</li>
+                            <li>Time: ${individualEvent.dates.start.localTime}</li>
+                            <li>Tickets: <a href="${individualEvent.url}">Click Here</a></li>
                         </ul>
                     </div>`;
-             });
-        document.getElementById("output").innerHTML = eventList;
-    })
-    .catch(error => console.log('error', error));
+        });
+    document.getElementById("output").innerHTML = eventList;    
+    });
+};
+
+eventsButton.addEventListener("click", function (event) {
+    event.preventDefault(); //prevents submit action which would reload page
+    getEvents();
 });
-       
-    
-    
-    
-    // const url = 'https://api.predicthq.com/v1/events';
-    // fetch(url)
-    // .then(function (response) {
-    //     return response.json();
-    // })
-    // .then(function (data) {
-    //     var eventList = data.results.map((individualEvent) => {
-    //         return `<div>
-    //                     <ul>
-    //                         <li>Name: ${individualEvent.title}</li>
-    //                         <li>Height: ${individualEvent.description}</li>
-    //                         <li>Mass: ${individualEvent.category}</li>
-    //                     </ul>
-    //                 </div>`;
-    //          });
-    //     document.getElementById("output").innerHTML = eventList;
-    // });
